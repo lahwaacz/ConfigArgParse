0417600bf25f347df2eab3bdf921ab823d8e2537
Jakub Klinkovský <j.l.k@gmx.com>
2015-08-07 10:05:05 +0200

    Use the reversed iterator, slicing creates copy of the list

--- configargparse.py
+++ configargparse.py
@@ -286,7 +286,7 @@ class ArgumentParser(argparse.ArgumentParser):
             config_streams = self._open_config_files(args)
 
         # parse each config file
-        for stream in config_streams[::-1]:
+        for stream in reversed(config_streams):
             try:
                 config_settings = self._config_file_parser.parse(stream)
             except ConfigFileParserException as e:
